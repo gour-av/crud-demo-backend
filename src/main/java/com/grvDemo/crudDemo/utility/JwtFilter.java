fbcb51cb8105810ed952a25579088e87e9cbdf51
gour-av <gouravguru97031@gmail.com>
2026-08-05 23:57:22 +0530

    fix for jwtfilter

--- src/main/java/com/grvDemo/crudDemo/utility/JwtFilter.java
+++ src/main/java/com/grvDemo/crudDemo/utility/JwtFilter.java
@@ -10,6 +10,10 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.ArrayList;
 
 @Component
 public class JwtFilter extends OncePerRequestFilter {
@@ -17,21 +21,54 @@ public class JwtFilter extends OncePerRequestFilter {
     @Autowired
     private JwtUtil jwtUtil;
 
+
     @Override
-    protected void doFilterInternal(HttpServletRequest request,
-                                    @NonNull HttpServletResponse response,
-                                    @NonNull FilterChain filterChain)
-            throws ServletException, IOException {
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+            @NonNull FilterChain filterChain
+    ) throws ServletException, IOException {
+
+
+        String path = request.getServletPath();
+
+
+        // Allow login and register without JWT
+        if(path.startsWith("/auth")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
 
         String authHeader = request.getHeader("Authorization");
 
-        if (authHeader != null && authHeader.startsWith("Bearer ")) {
+
+        if(authHeader != null && authHeader.startsWith("Bearer ")) {
+
             String token = authHeader.substring(7);
+
             String username = jwtUtil.extractUsername(token);
 
-            System.out.println("Authenticated user: " + username);
+
+            UsernamePasswordAuthenticationToken authentication =
+                    new UsernamePasswordAuthenticationToken(
+                            username,
+                            null,
+                            new ArrayList<>()
+                    );
+
+
+            SecurityContextHolder
+                    .getContext()
+                    .setAuthentication(authentication);
+
+
+            System.out.println(
+                    "Authenticated user: " + username
+            );
         }
 
-        filterChain.doFilter(request, response);
+
+        filterChain.doFilter(request,response);
     }
 }
